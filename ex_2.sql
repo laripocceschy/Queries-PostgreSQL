1eee2596b46e4dbe4abbb50dece097275a966576
Larissa <114714846+laripocceschy@users.noreply.github.com>
2022-10-28 22:03:21 -0300

    Add files via upload

---
+++ ex_2.sql
@@ -0,0 +1,5 @@
+/* Retorne todos os registros dos funcionários com 2 filhos. */
+
+select * from cap16."TB_FUNC" where numero_filhos = '2'
+
+select * from cap16."TB_FUNC" where cast(numero_filhos as integer) = 2
